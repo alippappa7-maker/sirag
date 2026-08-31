c4edb0ceb75777116a57a00699226a7219a62c63
alippappa7-maker <alippappa7@gmail.com>
2026-08-31 19:57:55 +0300

    Update NavItem.kt

--- app/src/main/java/com/siraj/app/core/navigation/NavItem.kt
+++ app/src/main/java/com/siraj/app/core/navigation/NavItem.kt
@@ -12,6 +12,6 @@ sealed class NavItem(val title: String, val icon: ImageVector, val route: String
     object Home : NavItem("الرئيسية", Icons.Default.Home, Screen.Home.route)
     object Studio : NavItem("الاستوديو", Icons.Default.CameraAlt, Screen.Studio.route)
     object Flashes : NavItem("فلاشات", Icons.Default.PlayCircle, "flashes")
-    object Library : NavItem("المكتبة", Icons.Default.LibraryBooks, Screen.Library.route)
+    object Library : NavItem("المكتبة", Icons.Default.LibraryBooks, Screen.Audio.route)
     object Mihrab : NavItem("المحراب", Icons.Default.Star, "mihrab")
 }
